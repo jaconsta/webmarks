36781c4620ad7cb25040ed58e36710e6b3967634
Javier Constain <constainopia@gmail.com>
2019-02-19 21:22:04 -0500

    Add post method

--- server/src/github.com/jaconsta/webmarks/main.go
+++ server/src/github.com/jaconsta/webmarks/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "encoding/json"
+  "io/ioutil"
   "log"
   "net/http"
 )
@@ -17,6 +18,10 @@ type Sites struct {
   Sites []Site `json:"sites"`
 }
 
+type OkResponse struct {
+  Message string `json:"message"`
+}
+
 var sites = Sites {
     Sites : []Site{
       Site{Name: "Google", Url: "https://www.google.com/", Rate: 3, Start: 4},
@@ -26,8 +31,14 @@ var sites = Sites {
     },
 }
 
+func appendToSlices(site Site) {
+  newSites := append(sites.Sites, site)
+  sites = Sites {
+    Sites: newSites,
+  }
+}
 
-func SitesHttpHandler(w http.ResponseWriter, r *http.Request) {
+func getSites(w http.ResponseWriter, r *http.Request) {
   res, err := json.Marshal(sites)
   if err != nil {
     log.Printf("Could not parse sites response")
@@ -36,6 +47,45 @@ func SitesHttpHandler(w http.ResponseWriter, r *http.Request) {
   }
   w.Header().Set("Content-type", "application/json")
   w.Write(res)
+}
+
+func addPost(w http.ResponseWriter, r *http.Request) {
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    http.Error(w, "Bad body", http.StatusInternalServerError)
+  }
+  // Serialize
+  var site *Site
+  json.Unmarshal(body, &site)
+  if err != nil {
+    http.Error(w, "Could not parse body", http.StatusInternalServerError)
+  }
+
+  //update sites list
+  appendToSlices(*site)
+
+  // Response
+  response := OkResponse{"Site added"}
+  addedResponse, err := json.Marshal(response)
+    if err != nil {
+      log.Printf("Could not write response")
+      http.Error(w, "Could not write response", http.StatusInternalServerError)
+      return
+    }
+  w.Header().Set("Content-type", "application/json")
+  w.Write(addedResponse)
+}
+
+func SitesHttpHandler(w http.ResponseWriter, r *http.Request) {
+  switch r.Method {
+    case http.MethodGet:
+      getSites(w, r)
+      return
+    case http.MethodPost:
+      addPost(w, r)
+      return
+
+  }
 }
 
 func main() {
