ec438a20ecfc88c83eb526ea71ee233a1976842f
Javier Constain <constainopia@gmail.com>
2019-05-30 23:01:25 -0500

    Update email service to use sendgrid

--- server/src/github.com/jaconsta/webmarks/services/email/email.go
+++ server/src/github.com/jaconsta/webmarks/services/email/email.go
@@ -3,41 +3,28 @@ package email
 import (
   "log"
   "fmt"
-  "net/smtp"
   "os"
+
+  "github.com/sendgrid/sendgrid-go"
+  "github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
 func SendEmail (userEmail string, code string) error {
   emailUser := os.Getenv("EMAIL_USER")
   emailSenderName := os.Getenv("EMAIL_SENDER_NAME")
-  emailPassword := os.Getenv("EMAIL_PASSWORD")
-  emailHost := os.Getenv("EMAIL_HOST")
-  emailSmtpSender := os.Getenv("EMAIL_SMTP_SENDER")
+  sendgridKey := os.Getenv("SENDGRID_API_KEY")
 
-  // Auth information
-  auth := smtp.PlainAuth(
-    "",
-    emailUser,
-    emailPassword,
-    emailHost,
-  )
-  message := fmt.Sprintf(
-    "From: %s<%s>\r\n" +
-    "To: %s\r\n" +
-    "Subject: Your access code\r\n" +
-    "<h1>Hello.<h1> <p>This is your code %s.</p>",
-    emailSenderName, emailUser, userEmail, code,
-  )
+  from := mail.NewEmail(emailSenderName, emailUser)
+  subject := "Your access code"
+  to := mail.NewEmail("Dear user", userEmail)
+  plainTextContent := "This is your code."
+  htmlContent := fmt.Sprintf("<h1>Hello.<h1> <p>This is your code %s.</p>", code)
+  message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+  client := sendgrid.NewSendClient(sendgridKey)
 
-  // Connect and send Email
-  err := smtp.SendMail(
-    emailSmtpSender,
-    auth,
-    emailUser,
-    []string{userEmail},
-    []byte(message),
-  )
-  if err != nil {
+  _, err := client.Send(message)
+  if err !=  nil {
+    log.Println("Could not send the authentication CODE email")
     log.Printf("%s", err)
   }
   return err
